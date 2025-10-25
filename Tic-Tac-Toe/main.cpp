d235c596ba7e53f63e5280600b853d07a90da527
Rajesh-Shaw <rkshaw7200@gmail.com>
2025-10-25 23:31:57 +0530

    Add Tic-Tac-Toe Game Project

---
+++ Tic-Tac-Toe/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3]; 
+
+void initializeBoard();
+void displayBoard();
+bool makeMove(char player);
+bool checkWin(char player);
+bool isDraw();
+
+
+int main() 
+{
+    return 0;
+}
